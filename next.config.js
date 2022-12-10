67f4ce9e2b6807dae76191163171f6e418f8207b
Roman Mkrtchian <mkrtchia@gmail.com>
2022-12-10 12:41:40 +0100

    chore: make next app exportable to static files

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const withNextra = require("nextra")({
-  reactStrictMode: true,
-  swcMinify: true,
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
 
-module.exports = withNextra();
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = {
+  ...withNextra(),
+  ...nextConfig,
+};
