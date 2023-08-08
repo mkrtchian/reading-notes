d5c7370fbdd0f52092fd6fcdd676b2476cc4f136
Roman Mkrtchian <mkrtchia@gmail.com>
2023-08-08 08:46:26 +0200

    chore: refactor _app.tsx to _app.mdx

--- pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import { AppProps } from "next/app";
-import "../styles.css";
-
-export default function ReadingNotes({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
