b1f1da6390580c432607e439395805b1b64d6c7e
Roman Mkrtchian <mkrtchia@gmail.com>
2023-08-29 00:12:45 +0200

    content(reinventing organizations): add colors about stages

--- components/markers/Colors.tsx
+++ components/markers/Colors.tsx
@@ -1,47 +1,23 @@
 export function Orange({ children }: { children: React.ReactNode }) {
-  return (
-    <strong style={{ color: "#ff9900" }}>
-      {children}
-    </strong>
-  );
+  return <strong style={{ color: "#ff9900" }}>{children}</strong>;
 }
 
 export function Pink({ children }: { children: React.ReactNode }) {
-  return (
-    <strong style={{ color: "#e06666" }}>
-      {children}
-    </strong>
-  );
+  return <strong style={{ color: "#e06666" }}>{children}</strong>;
 }
 
 export function Blue({ children }: { children: React.ReactNode }) {
-  return (
-    <strong style={{ color: "#4a86e8" }}>
-      {children}
-    </strong>
-  );
+  return <strong style={{ color: "#4a86e8" }}>{children}</strong>;
 }
 
 export function Yellow({ children }: { children: React.ReactNode }) {
-  return (
-    <strong style={{ color: "#bf9000" }}>
-      {children}
-    </strong>
-  );
+  return <strong style={{ color: "#bf9000" }}>{children}</strong>;
 }
 
 export function Purple({ children }: { children: React.ReactNode }) {
-  return (
-    <strong style={{ color: "#674ea7" }}>
-      {children}
-    </strong>
-  );
+  return <strong style={{ color: "#674ea7" }}>{children}</strong>;
 }
 
 export function Green({ children }: { children: React.ReactNode }) {
-  return (
-    <strong style={{ color: "#6aa84f" }}>
-      {children}
-    </strong>
-  );
+  return <strong style={{ color: "#6aa84f" }}>{children}</strong>;
 }
---
+++ components/markers/reinventingOrganizations/Colors.tsx
@@ -0,0 +1,27 @@
+export function Infrarouge({ children }: { children: React.ReactNode }) {
+  return <strong style={{ color: "#660000" }}>{children}</strong>;
+}
+
+export function Magenta({ children }: { children: React.ReactNode }) {
+  return <strong style={{ color: "#990000" }}>{children}</strong>;
+}
+
+export function Rouge({ children }: { children: React.ReactNode }) {
+  return <strong style={{ color: "#cc0000" }}>{children}</strong>;
+}
+
+export function Ambre({ children }: { children: React.ReactNode }) {
+  return <strong style={{ color: "#e06666" }}>{children}</strong>;
+}
+
+export function Orange({ children }: { children: React.ReactNode }) {
+  return <strong style={{ color: "#e69138" }}>{children}</strong>;
+}
+
+export function Vert({ children }: { children: React.ReactNode }) {
+  return <strong style={{ color: "#38761d" }}>{children}</strong>;
+}
+
+export function Opale({ children }: { children: React.ReactNode }) {
+  return <strong style={{ color: "#45818e" }}>{children}</strong>;
+}
